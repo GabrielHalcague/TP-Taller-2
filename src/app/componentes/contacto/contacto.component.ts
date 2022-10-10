027ee60f58b74961cf94d988ca244677869f9583
Gabriel Halcague <42984507+GabrielHalcague@users.noreply.github.com>
2022-10-10 19:20:36 -0300

    1

--- src/app/componentes/contacto/contacto.component.ts
+++ src/app/componentes/contacto/contacto.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  datos!:string;
+  formGroup!: FormGroup;
+  constructor( protected router:Router, private formBuilder: FormBuilder, protected httpClient: HttpClient){}
 
-  constructor() { }
-
+  
   ngOnInit(): void {
-  }
+    console.log('iniciando compoenent');
+    this.formGroup= new FormGroup({
+      username: new FormControl('', Validators.required),
+      useremail: new FormControl('',Validators.email),
+      usermensaje: new FormControl('',Validators.required)
 
+    });    
+  }  
+  onSubmit() {
+    this.datos=this.formGroup.value.username;
+  }
 }
